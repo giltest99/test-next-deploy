6831cf357928518b97446c7cc0d3e7665c4688e0
Gilles <sellig.blater@free.fr>
2023-04-10 10:09:44 +0200

    Add doc page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center px-24 py-6">
       <h1 className="text-4xl font-bold">Hello World !</h1>
       <h2>Test de déploiement Next Js 13</h2>
     </main>
