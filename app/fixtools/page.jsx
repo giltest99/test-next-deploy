18b01bef82ac810b698ba048c09f0d1665322a78
Gilles <sellig.blater@free.fr>
2023-04-10 19:06:52 +0200

    Add concatenate function

--- app/fixtools/page.jsx
+++ app/fixtools/page.jsx
@@ -6,15 +6,25 @@ export default function FixTools() {
   const sourceRef = useRef();
   const outputRef = useRef();
 
-  const testOutput = () => {
-    alert("Cliqué !");
-  };
-
   const concatValues = () => {
     const sourceValue = sourceRef.current.value;
-    if (!sourceValue) return;
-    alert(sourceValue);
+    if (!sourceValue) {
+      sourceRef.current.focus();
+      return;
+    }
+    //alert(sourceValue);
+
+    const source = sourceRef.current.value;
+    const sourceSplit = source.split("\n");
+    const outputArray = [];
+    sourceSplit.forEach((element) => {
+      if (element.trim() !== "") outputArray.push(element.trim());
+      else return;
+    });
+    console.log(outputArray);
+    outputRef.current.value = outputArray;
   };
+
   const resetSource = () => {
     sourceRef.current.value = "";
     sourceRef.current.focus();
@@ -37,6 +47,7 @@ export default function FixTools() {
           <button
             className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-1 rounded duration-300"
             onClick={concatValues}
+            text="Concatenate values"
           >
             Concatenate
           </button>
