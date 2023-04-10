5ca3b18abd27613a94edc8878101718ae711f5ed
Gilles <sellig.blater@free.fr>
2023-04-10 17:50:25 +0200

    Create fix tools form. Client component

--- app/doc/page.jsx
+++ app/doc/page.jsx
@@ -1,9 +1,13 @@
 
 
+
 export default function Documentation() {
+
+
   return (
     <main className="flex min-h-screen flex-col items-center px-24 py-6">
       <h1 className="text-4xl font-bold">Documentation</h1>
+      
     </main>
   );
 }
--- app/fixtools/page.jsx
+++ app/fixtools/page.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+"use client";
+
+import { useRef } from "react";
 
 export default function FixTools() {
+  const sourceRef = useRef();
+  const outputRef = useRef();
+
+  const testOutput = () => {
+    alert("Cliqué !");
+  };
+
+  const concatValues = () => {
+    const sourceValue = sourceRef.current.value;
+    if (!sourceValue) return;
+    alert(sourceValue);
+  };
+  const resetSource = () => {
+    sourceRef.current.value = "";
+    sourceRef.current.focus();
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center px-24 py-6">
-        <h1 className="text-4xl font-bold">Fix Tools</h1>
+      <h1 className="text-4xl font-bold">Fix Tools</h1>
+      <section className="flex flex-col gap-4 my-4 container">
+        <div className="flex flex-col gap-2 my-4">
+          <label htmlFor="source">Source</label>
+          <textarea
+            ref={sourceRef}
+            id="source"
+            className="min-h-[200px] p-4 resize-none rounded"
+            autoFocus
+          ></textarea>
+        </div>
+        <div className="flex gap-4">
+          <button
+            className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-1 rounded duration-300"
+            onClick={concatValues}
+          >
+            Concatenate
+          </button>
+          <button
+            className="bg-red-600 hover:bg-red-700 text-white px-4 py-1 rounded duration-300"
+            onClick={resetSource}
+          >
+            Reset source
+          </button>
+        </div>
+        <div className="flex flex-col gap-2 my-4 container">
+          <label htmlFor="output">Output</label>
+          <textarea
+            ref={outputRef}
+            id="output"
+            className="min-h-[200px] p-4 resize-none rounded"
+          ></textarea>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
