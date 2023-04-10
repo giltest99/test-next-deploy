c092688b2619733fc02a892798c2a4ff46968e1e
Gilles <sellig.blater@free.fr>
2023-04-10 14:53:42 +0200

    Create Navbar & add to main layout

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Navigation() {
+  return (
+    <nav className='flex flex-row justify-center align-middle bg-neutral-100 py-2'>
+        <ul className='flex flex-row gap-4'>
+            <Link href="/" className='hover:bg-blue-200 px-6 py-1 rounded duration-300 font-semibold'>Home</Link>
+            <Link href="/doc" className='hover:bg-blue-200 px-6 py-1 rounded duration-300 font-semibold'>Doc</Link>
+        </ul>
+    </nav>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -3,14 +3,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center px-24 py-6">
       <h1 className="text-4xl font-bold">Hello World !</h1>
       <h2>Test de déploiement Next Js 13</h2>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id itaque
-        sapiente quo corporis placeat atque sunt ipsam aliquam facere quis
-        voluptas facilis est possimus porro doloribus quia voluptatibus
-        delectus, quasi fugiat voluptate numquam? Non vitae omnis nihil quas
-        esse, animi vel a consequatur tempora, tempore consequuntur maxime iste
-        cum maiores!
-      </p>
     </main>
   );
 }
